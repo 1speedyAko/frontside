4244214fdcb903d4929c47cda6adc5aaedaca822
charlie <charloako1@gmail.com>
2024-07-24 20:31:43 -0700

    self typing added

--- app/body/page.js
+++ app/body/page.js
@@ -2,13 +2,17 @@
 
 import React from "react";
 import Link from 'next/link';
+import { ReactTyped } from "react-typed";
 
 const Body = () => {
   return (
     <div className="w-full flex flex-col lg:flex-row p-4">
       <div className="w-full lg:w-1/2 p-4">
         <p className="text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.
+          lorem ipsum {' '}
+          <ReactTyped
+          strings={['lorem ipsum dolisad saldnsad jsadksad']} typeSpeed={100} loop 
+          />
         </p>
       </div>
       <div className="w-full lg:w-1/2 flex justify-center items-center p-4">
