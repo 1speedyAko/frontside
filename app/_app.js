329494f97fe92e50e9f3664bda2509177d600f16
charlie <charloako1@gmail.com>
2024-08-12 22:57:30 +0300

    cryptomus meta tag

--- app/_app.js
+++ app/_app.js
@@ -1,7 +1,6 @@
-// pages/_app.js
-
 import './globals.css';
 import { Roboto } from '@next/font/google';
+import Head from 'next/head';
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -10,10 +9,17 @@ const roboto = Roboto({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={roboto.className}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        {/* Cryptomus domain verification meta tag */}
+        <meta name="cryptomus-verification" content="d4150844" />
+      </Head>
+      <div className={roboto.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
+
 export { roboto };
 export default MyApp;
