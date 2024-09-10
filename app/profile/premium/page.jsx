d979563752bb81fe696c9655a384d503bc0af583
charlie <charloako1@gmail.com>
2024-09-10 18:27:45 +0300

    users api test

--- app/profile/premium/page.jsx
+++ app/profile/premium/page.jsx
@@ -24,7 +24,7 @@ const Premium = () => {
 
     const fetchUserDetails = async () => {
       try {
-        const response = await axios.get(`${API_URL}/users/me/`, {
+        const response = await axios.get(`${API_URL}/users/auth/users/me/`, {
           headers: { Authorization: `Bearer ${token}` },
         });
         return response.data;
