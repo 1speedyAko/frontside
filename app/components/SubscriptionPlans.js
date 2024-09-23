c7d47a14ca3679ee2fc2caf127b035b07d168df4
charlie <charloako1@gmail.com>
2024-09-23 18:48:50 +0300

    capitalize category

--- app/components/SubscriptionPlans.js
+++ app/components/SubscriptionPlans.js
@@ -8,10 +8,15 @@ const SubscriptionPlans = ({ plans }) => {
       key={plan.category} 
       className="p-6 bg-ebony shadow-md rounded-lg border border-gray-400 hover:prime transition-all duration-300"
     >
-      <p className="prime text-2xl">{plan.currency}{plan.price}</p>
-      <h3 className="text-xl font-bold prime">{plan.category} Plan</h3>
-      
+      <p className="prime text-2xl">{plan.currency}{plan.price && <p className="text-sm text-gray-300 font-bold">{plan.description}</p>}</p>
+      <h3 className="text-xl font-bold prime capitalize">{plan.category} Plan</h3>
       <p className="prime font-bold">{plan.description}</p>
+      <div>
+        <ul>
+          <li className='text-prime text-xl'>{plan.info_1}</li>
+          <li className='text-prime text-xl'>{plan.info_2}</li>
+        </ul>
+      </div>
       {plan.discount && <p className="text-gray-400">Save {plan.currency}{plan.discount}</p>}
       <Link href={`/subscriptions/${plan.category}`}>
         <a className="bg-indigo-600 text-white py-2 px-4 rounded-lg mt-4 inline-block">
