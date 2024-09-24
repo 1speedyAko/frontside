79a0234f6ef2ac1bc72fa9f75f22ead48ce10d9f
charlie <charloako1@gmail.com>
2024-09-24 16:07:22 +0300

    added post

--- app/subscriptions/[plan_name]/page.jsx
+++ app/subscriptions/[plan_name]/page.jsx
@@ -12,28 +12,24 @@ export default function PlanPaymentPage({ params }) {
   useEffect(() => {
     const initiatePayment = async () => {
       try {
-        // Retrieve the JWT token from localStorage
         const token = localStorage.getItem('access');
 
-        // Make a request to the server-side API to initiate the payment
-        const response = await axios.post(`/api/subscriptions/${plan_name}`, {}, {
+        const response = await axios.post(`/api/subscriptions/${plan_name}`, {
           headers: {
             'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
           },
         });
 
-        if (response.status === 200 && response.data.payment_url) {
-          // Redirect user to the checkout page
+        if (response.data.payment_url) {
           window.location.href = response.data.payment_url;
         } else {
           alert('Failed to initiate payment.');
-          router.push('/subscriptions'); // Redirect back if payment initiation fails
+          router.push('/subscriptions');
         }
       } catch (error) {
-        console.error('Error initiating payment:', error);
+        console.error(error);
         alert('An error occurred while processing your payment.');
-        router.push('/subscriptions'); // Redirect in case of any errors
+        router.push('/subscriptions');
       }
     };
 
